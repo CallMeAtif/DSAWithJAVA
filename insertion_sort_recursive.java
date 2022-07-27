49d7444bd1f636ccecd846075794c4afa303ce37
Atif <Atif472005@gmail.com>
2022-07-27 20:27:17 +0530

    MessingAroundWithAlgorithms

---
+++ insertion_sort_recursive.java
@@ -0,0 +1,28 @@
+package com.company;
+
+public class insertion_sort_recursive {
+    public static void insertion_recur(int [] arr, int numItems){
+        if(numItems < 2)
+            return;
+
+        insertion_recur(arr,numItems - 1);
+
+        int newElement = arr[numItems - 1];
+
+        int i;
+
+        for(i = numItems - 1; i > 0 && arr[i - 1] > newElement; i--){
+            arr[i] = arr[i - 1];
+        }
+        arr[i] = newElement;
+    }
+    public static void main(String[] args) {
+        int [] arr = {23,-2,3,-22,9,-1,0};
+
+        insertion_recur(arr, arr.length);
+
+        for(int e: arr){
+            System.out.println(e);
+        }
+    }
+}
---
+++ merge_sort_decending_order.java
@@ -0,0 +1,41 @@
+package com.company;
+
+public class merge_sort_decending_order {
+    public static void mergeSort(int [] arr, int start, int end){
+        if(end - start < 2)
+            return;
+
+        int mid = (end + start) / 2;
+
+        mergeSort(arr, start, mid);
+        mergeSort(arr, mid, end);
+        merge(arr, start, mid, end);
+    }
+
+    public static void merge(int [] arr, int start ,int mid , int end){
+        if(arr[mid - 1] >= arr[mid]){
+            return;
+        }
+
+        int i = start;
+        int j = mid;
+        int tempIndex = 0;
+
+        int [] temp = new int [end - start];
+        while(i < mid && j < end){
+            temp[tempIndex++] = arr[i] >= arr[j] ? arr[i++] : arr[j++];
+        }
+
+        System.arraycopy(arr, i, arr, start + tempIndex, mid-i);
+        System.arraycopy(temp, 0, arr, start, tempIndex);
+    }
+    public static void main(String[] args) {
+        int [] arr = {2,-2,53,62,-51,0,8};
+
+        mergeSort(arr, 0, arr.length);
+
+        for(int i = 0; i< arr.length;i++){
+            System.out.println(arr[i]);
+        }
+    }
+}

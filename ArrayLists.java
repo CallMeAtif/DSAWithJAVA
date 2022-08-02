830e2bfd357eb6ca602be2256996700edd942d7f
Atif <Atif472005@gmail.com>
2022-08-02 22:01:02 +0530

    second commit

--- ArrayLists.java
+++ ArrayLists.java
@@ -1,30 +1,27 @@
 package com.company;
-import javax.swing.plaf.multi.MultiLabelUI;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class ArrayLists {
     public static void main(String[] args) {
 
-        List<Employee_ArrayList> employees = new ArrayList<>();
+        List<Employee> employees = new ArrayList<>();
 
-        employees.add(new Employee_ArrayList("Atif", "Mulla", 123));
-        employees.add(new Employee_ArrayList("Arman", "Bhakshey",456));
-        employees.add(new Employee_ArrayList("Abhishek","Singh",789));
+        employees.add(new Employee("Atif", "Mulla", 123));
+        employees.add(new Employee("Arman", "Bhakshey",456));
+        employees.add(new Employee("Abhishek","Singh",789));
 
-//        employees.forEach(employee_arrayList -> System.out.println(employee_arrayList));
+        employees.forEach(Employee -> System.out.println(Employee));
 
-//        employees.forEach(employee_arrayList -> System.out.println(employee_arrayList));
 
-        Employee_ArrayList[] employeeArray = employees.toArray(new Employee_ArrayList[employees.size()]);
-        for(Employee_ArrayList e: employeeArray){
+        Employee[] employeeArray = employees.toArray(new Employee[employees.size()]);
+        for(Employee e: employeeArray){
             System.out.println(e);
         }
 
-        System.out.println(employees.contains(new Employee_ArrayList("Arman","Bhakshey", 456)));
+        System.out.println(employees.contains(new Employee("Arman","Bhakshey", 456)));
 
-        System.out.println(employees.indexOf(new Employee_ArrayList("Abhishek", "Singh",789)));
+        System.out.println(employees.indexOf(new Employee("Abhishek", "Singh",789)));
     }
 
 }
